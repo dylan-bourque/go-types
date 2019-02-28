607ce763424c56b1d987ff73c18a549654f14ec9
Dylan Bourque <dylan.m.bourque@gmail.com>
2019-02-28 10:43:55 -0600

    corrected module name to match repo path

--- date/date_test.go
+++ date/date_test.go
@@ -2,24 +2,22 @@
 //
 // Use of this source code is governed by the MIT open source license that can be found in the LICENSE file.
 
-package date_test
+package date
 
 import (
 	"testing"
 	"time"
-
-	"github.com/dylan-bourque/types/date"
 )
 
 func TestXxx(t *testing.T) {
 	now := time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
 	srcYear, srcMonth, srcDay := now.Date()
-	d, e := date.FromUnits(srcYear, int(srcMonth), srcDay)
+	d, e := FromUnits(srcYear, int(srcMonth), srcDay)
 	if e != nil {
 		t.Errorf("Expected no error, got %v", e)
 	} else {
 
-		year, month, day := date.ToUnits(d)
+		year, month, day := ToUnits(d)
 		if year != 2000 || month != 1 || day != 1 {
 			t.Errorf("Expected 2000-01-01, got %04v-%02v-%02v", year, month, day)
 		}
@@ -65,12 +63,12 @@ func TestFromUnits(tt *testing.T) {
 	}
 	for _, tc := range cases {
 		tt.Run(tc.name, func(t *testing.T) {
-			dt, e := date.FromUnits(tc.year, tc.month, tc.day)
+			dt, e := FromUnits(tc.year, tc.month, tc.day)
 			if tc.valid {
 				if e != nil {
 					t.Errorf("Unexpected failure: %v", e)
 				} else {
-					y, m, d := date.ToUnits(dt)
+					y, m, d := ToUnits(dt)
 					if y != tc.year || m != tc.month || d != tc.day {
 						t.Errorf("Expected %04d-%02d-%02d, got %04d-%02d-%02d", tc.year, tc.month, tc.day, y, m, d)
 					}
@@ -79,8 +77,8 @@ func TestFromUnits(tt *testing.T) {
 				if e == nil {
 					t.Errorf("Unexpected success")
 				}
-				y, m, d := date.ToUnits(dt)
-				if dt != date.Nil {
+				y, m, d := ToUnits(dt)
+				if dt != Nil {
 					t.Errorf("Expected date.Nil, got %04d-%02d-%02d", y, m, d)
 				}
 			}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/dylan-bourque/types
+module github.com/dylan-bourque/go-types
 
 require github.com/pkg/errors v0.8.1
